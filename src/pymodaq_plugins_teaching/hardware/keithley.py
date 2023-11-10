953f3853c7ffcf5202ca7d5862f5005cd4e16d55
Sébastien Weber <seba.weber@gmail.com>
2023-11-10 10:56:30 +0100

    both real and mock keithley are done

--- src/pymodaq_plugins_teaching/hardware/keithley.py
+++ src/pymodaq_plugins_teaching/hardware/keithley.py
@@ -9,6 +9,7 @@ import warnings
 from pymodaq_plugins_teaching.hardware.serial_addresses import SerialAddresses, BaseEnum
 import random
 from pylablib.core.devio import SCPI, interface
+from pylablib.devices.Keithley.multimeter import TGenericFunctionParameters
 
 
 class EnumParameterClass(interface.EnumParameterClass):
@@ -17,6 +18,12 @@ class EnumParameterClass(interface.EnumParameterClass):
 
     def names(self):
         list(self._get_alias_map().keys())
+
+
+class Measurement(BaseEnum):
+    volt_dc = 0
+    curr_dc = 1
+    freq_volt = 2
 
 
 class ResourceManager:
@@ -39,9 +46,13 @@ class Keithley2110:
                                                {"volt_dc": "VOLT:DC", "curr_dc": "CURR:DC",
                                                 "freq_volt": "FREQ:VOLT", "none": "NONE"})
 
-
+    measurement = Measurement['volt_dc']
 
     def __init__(self, address: str = None):
+
+        self._resolution: float = 1e-5
+        self._auto: bool = True
+        self._range: float = 0.1
 
         self._is_open = False
         if address is not None:
@@ -49,9 +60,11 @@ class Keithley2110:
 
     @property
     def is_open(self):
+        """ Get the communication status with the instrument"""
         return self._is_open
 
     def open_communication(self, address: str):
+        """ Open a communication channel with the instrument using the serial address (USb, GPIB,...)"""
         if self.is_open:
             raise IOError('Device already connected')
         else:
@@ -61,26 +74,77 @@ class Keithley2110:
                 self._is_open = True
 
     def close(self):
+        """ Close de communication channel"""
         if self._is_open:
             self._is_open = False
 
-    def get_function(self):
-        return self._measurement.name
+    def get_function(self, channel='primary'):
+        """ Get the current measurement type"""
+        if not self.is_open:
+            raise TimeoutError
+        return self.measurement.name
 
-    def set_function(self, function: str):
-        if function not in self._measurements.names():
+    def set_function(self, function: str, channel="primary", reset_secondary=True):
+        """ Set a measurement type
+
+        Parameters
+        ----------
+        function: str
+            One of the possible measurement, see the Measurement enum
+        """
+        if not self.is_open:
+            raise TimeoutError
+        if function not in self.measurements.names():
             warnings.warn(f'The requested measurement, {function} cannot be set')
         else:
-            self._measurement = self._measurements[function]
-
-    def get_range(self) -> float:
-        return self._range.value
+            self.measurement = Measurement[function]
 
     def get_reading(self, channel='primary'):
-        return self.get_range() * random.random()
+        """ Grab the current reading from the device"""
+        if not self.is_open:
+            raise TimeoutError
+        return int(self._range * random.random() / self._resolution) * self._resolution
 
-    def get_range(self):
+    def reset(self):
+        if not self.is_open:
+            raise TimeoutError
         pass
+
+    def get_id(self):
+        """ Get info about the connected device """
+        if not self.is_open:
+            raise TimeoutError
+        return 'KEITHLEY INSTRUMENTS INC.,MODEL 2110,1417097,02.01-02-01'
+
+    def get_function_parameters(self, function: str) -> TGenericFunctionParameters:
+        """ Get the parameters of the current measurement: range, resolution, autorange"""
+        if not self.is_open:
+            raise TimeoutError
+        return TGenericFunctionParameters(self._range, self._resolution, self._auto)
+
+    def set_function_parameters(self, function: str, **kwargs):
+        """ Set the parameters of a given measurement
+
+        Parameters
+        ----------
+        function: str
+            one of the possible measurement
+        kwargs: dict
+            mapping of the possible parameters: 'rng', 'resolution', 'autorng'
+        Returns
+        -------
+        TGenericFunctionParameters: the current set parameters
+        """
+        if not self.is_open:
+            raise TimeoutError
+        for kwarg in kwargs:
+            if kwarg == 'rng':
+                self._range = kwargs[kwarg]
+            elif kwarg == 'autorng':
+                self._auto = kwargs[kwarg]
+            elif kwarg == 'resolution':
+                self._resolution = kwargs[kwarg]
+        return self.get_function_parameters(function)
 
 
 if __name__ == '__main__':
